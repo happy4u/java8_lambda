f05272b3964693de4fd2d122e9323b413a4c6b2f
dannyLee <happy4u@gmail.com>
2016-06-28 00:21:18 +0900

    BiFunction example 추가

--- src/BasicLambdasExamples.java
+++ src/BasicLambdasExamples.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 
 public class BasicLambdasExamples {
 
@@ -7,15 +8,24 @@ public class BasicLambdasExamples {
         String[] atp = {"Rafael Nadal", "Novak Djokovic", "Stanislas Wawrinka", "David Ferrer", "Roger Federer", "Andy Murray", "Tomas Berdych", "Juan Martin Del Potro"};
         List<String> players =  Arrays.asList(atp);
 
-        // Old looping
+        System.out.println("Old looping");
+        System.out.println("------------------------------------");
         for (String player : players) {
             System.out.print(player + "; ");
         }
 
-        // Using lambda expression and functional operations
+        System.out.println("\n\n\nUsing lambda expression and functional operations");
+        System.out.println("------------------------------------");
         players.forEach((player) -> System.out.print(player + "; "));
 
-        // Using double colon operator in Java 8
+
+        System.out.println("\n\n\nUsing double colon operator in Java 8");
+        System.out.println("------------------------------------");
         players.forEach(System.out::println);
+
+        System.out.println("\n\n\n\n");
+        BiFunction<Integer, Integer, Integer> addition = (x, y) -> x + y;
+        System.out.println( "calling addition of 2 and 3 resulting: " + addition.apply( 2, 3 ) );
+
     }
 }
